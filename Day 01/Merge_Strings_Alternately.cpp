f0e8a2bd05cb1b5d03bcc8ba51827a6e1f326b85
Tariq Mushtaq <142487186+taariqmushtaq@users.noreply.github.com>
2024-04-13 01:53:09 +0500

    Update Merge_Strings_Alternately.cpp

--- Day 01/Merge_Strings_Alternately.cpp
+++ Day 01/Merge_Strings_Alternately.cpp
@@ -8,9 +8,12 @@ public:
             result += word1[i++];
             result += word2[j++];
         }
+        // agr aik word ka size chota bh ha to, upr wali condition mai nahi ayy ga, to isliye check k liyy aik or loop lga lia word1 k liyy.
         while (i < word1.size()) {
             result += word1[i++];
         }
+
+        // same agr dusray word ka size chota bh ha to, upr waly phly loop ki condition mai nahi ayy ga, to isliye check k liyy aik or loop lga lia word2 k liyy.
         while (j < word2.size()) { // Corrected loop condition
             result += word2[j++];
         }
