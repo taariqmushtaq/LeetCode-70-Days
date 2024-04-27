16fdcc7ce0295bf8aea4584f70d1d41f82b35a57
Tariq Mushtaq <tariq2770873@gmail.com>
2024-04-27 23:53:29 +0500

    new added

---
+++ Day 16/reverse_linklist.cpp
@@ -0,0 +1,18 @@
+
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        ListNode* prev = nullptr;
+        ListNode* curr = head;
+        ListNode* next = nullptr;
+
+        while (curr != nullptr) {
+            next = curr->next;
+            curr->next = prev;
+            prev = curr;
+            curr = next;
+        }
+
+        return prev;
+    }
+};
