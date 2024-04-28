f4603176c2efceacaaa539fbffb1cc14005c0139
Tariq Mushtaq <tariq2770873@gmail.com>
2024-04-28 23:46:14 +0500

    new added

---
+++ Day 17/add_numbers.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+        ListNode dummy(0);
+        ListNode* curr = &dummy;
+        int carry = 0;
+        
+        while (l1 || l2 || carry) {
+            int sum = carry;
+            if (l1) {
+                sum += l1->val;
+                l1 = l1->next;
+            }
+            if (l2) {
+                sum += l2->val;
+                l2 = l2->next;
+            }
+            carry = sum / 10;
+            curr->next = new ListNode(sum % 10);
+            curr = curr->next;
+        }
+        
+        return dummy.next;
+    }
+};
